bd4a6db5dbe12a2a4baf4c8ffb764c103e765360
Michael Haraeb <haraebm@gmail.com>
2021-05-27 16:29:12 -0300

    Second commit, board and simple play done

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ row_1 = ["⬜","⬜","⬜"]
 row_2 = ["⬜","⬜","⬜"]
 row_3 = ["⬜","⬜","⬜"]
 
+
+def check_score():
+	return row_1[0] == "x" and row_1[1] == "x" and row_1[2] == "x"
+		
+
 def board():
 	board = f"{row_1},\n{row_2},\n{row_3}"
 	print(board)
-
 
 
 def playgame(user):
@@ -34,10 +38,22 @@ def playgame(user):
 		
 	board()
 
-playgame(0)
 
-# Way to print that board
-# Create 2 players
-# Select random player to go first (coin toss)
-#
+count = 0 
+game_on = True
+while game_on:
+	count += 1
+	user = int(input("Enter your position:"))
+	playgame(user)
+	if check_score():
+		game_on = False
+		print("You won")
+	elif count == 9:
+		print("you ran out of options")
+		game_on = False
+
+
+
+
+
 
